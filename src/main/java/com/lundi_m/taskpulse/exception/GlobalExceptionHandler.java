b986c45ed001a78fed9f5f8e916e1199b2e44651
Lundi <keabetswelundim@gmail.com>
2026-06-19 16:36:14 +0200

    add global exception handling

---
+++ src/main/java/com/lundi_m/taskpulse/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.lundi_m.taskpulse.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(fieldError -> errors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
