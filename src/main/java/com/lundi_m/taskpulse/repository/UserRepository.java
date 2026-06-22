db54455dee89d0e259e62dc167bc40efa72319e6
Lundi <keabetswelundim@gmail.com>
2026-06-22 12:27:25 +0200

    add @Repository annotation

--- src/main/java/com/lundi_m/taskpulse/repository/UserRepository.java
+++ src/main/java/com/lundi_m/taskpulse/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.lundi_m.taskpulse.repository;
 
 import com.lundi_m.taskpulse.model.TaskPulseUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<TaskPulseUser, Long> {
 
     Optional<Long> findByEmail(String email);
