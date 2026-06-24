dde1c48471cb09fa4c2b24148628ec05fed438d3
Lundi <keabetswelundim@gmail.com>
2026-06-24 17:17:47 +0200

    implement refresh token and logout functionality

---
+++ src/main/java/com/lundi_m/taskpulse/dto/RefreshRequest.java
@@ -0,0 +1,8 @@
+package com.lundi_m.taskpulse.dto;
+
+import lombok.Data;
+
+@Data
+public class RefreshRequest {
+    String refreshToken;
+}
---
+++ src/main/java/com/lundi_m/taskpulse/model/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.lundi_m.taskpulse.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private TaskPulseUser user;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+}
