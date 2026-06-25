8f1e861ba7bbd8ca06c1630a38d65b9eff89b6e0
Lundi <keabetswelundim@gmail.com>
2026-06-25 12:30:34 +0200

    add @Repository annotation

--- src/main/java/com/lundi_m/taskpulse/repository/RefreshTokenRepository.java
+++ src/main/java/com/lundi_m/taskpulse/repository/RefreshTokenRepository.java
@@ -2,9 +2,11 @@ package com.lundi_m.taskpulse.repository;
 
 import com.lundi_m.taskpulse.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
     void deleteByToken(String token);
