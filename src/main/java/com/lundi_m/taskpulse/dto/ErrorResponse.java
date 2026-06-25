c587244022d56384d55c6e6867fb54aa713ad606
Lundi <keabetswelundim@gmail.com>
2026-06-25 20:08:21 +0200

    add error response dto

---
+++ src/main/java/com/lundi_m/taskpulse/dto/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.lundi_m.taskpulse.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+}
