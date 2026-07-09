7abe0c07d1eac504bd5cf2ad07014f17fcc08478
Lundi <keabetswelundim@gmail.com>
2026-07-09 16:16:12 +0200

    feature: add InvalidTokenException

---
+++ src/main/java/com/lundi_m/taskpulse/exception/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package com.lundi_m.taskpulse.exception;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
