48ad1871121d5d8076d67ffe19b1f1bc9db8f302
Lundi <keabetswelundim@gmail.com>
2026-07-22 13:33:20 +0200

    test: add unit tests for ScoringEngine

---
+++ src/test/java/com/lundi_m/taskpulse/recommendation/ScoringEngineTest.java
@@ -0,0 +1,226 @@
+package com.lundi_m.taskpulse.recommendation;
+
+import com.lundi_m.taskpulse.model.entity.MoodEntry;
+import com.lundi_m.taskpulse.model.entity.Task;
+import com.lundi_m.taskpulse.model.enums.DifficultyLevel;
+import com.lundi_m.taskpulse.model.enums.EnergyLevel;
+import com.lundi_m.taskpulse.model.enums.MoodType;
+import com.lundi_m.taskpulse.model.enums.Priority;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ScoringEngineTest {
+
+    private ScoringEngine scoringEngine;
+
+    @BeforeEach
+    void setUp(){
+        scoringEngine = new ScoringEngine();
+    }
+
+    private MoodEntry createMood(MoodType moodType, EnergyLevel energyLevel, int availableTime){
+        return  MoodEntry.builder()
+                .moodType(moodType)
+                .energyLevel(energyLevel)
+                .availableTime(availableTime)
+                .build();
+    }
+
+    private Task createTask(String title,
+                            DifficultyLevel difficulty,
+                            Priority priority,
+                            int estimatedDuration,
+                            LocalDate deadline) {
+        return Task.builder()
+                .title(title)
+                .difficultyLevel(difficulty)
+                .priority(priority)
+                .estimatedDuration(estimatedDuration)
+                .deadline(deadline)
+                .completed("Not completed")
+                .build();
+    }
+
+    @Test
+    void shouldRecommendVeryEasyTaskForExhaustedAndDrainedUser(){
+
+        MoodEntry mood = createMood(MoodType.EXHAUSTED, EnergyLevel.DRAINED, 60);
+
+        Task easyTask = createTask("easy",
+                DifficultyLevel.EASY,
+                Priority.LOW,
+                30,
+                null);
+        Task veryEasyTask = createTask("very easy",
+                DifficultyLevel.VERY_EASY,
+                Priority.LOW,
+                50,
+                null);
+        Task hardTask = createTask("hard",
+                DifficultyLevel.HARD,
+                Priority.HIGH,
+                40,
+                null);
+
+        RecommendationScore recommendation = scoringEngine.recommend(List.of(easyTask, veryEasyTask, hardTask), mood)
+                .orElseThrow();
+
+        assertEquals("very easy", recommendation.getTask().getTitle());
+    }
+
+    @Test
+    void shouldRecommendEasyOrMediumTaskForExhaustedAndPeakUser(){
+
+        MoodEntry mood = createMood(MoodType.EXHAUSTED, EnergyLevel.PEAK, 60);
+
+        Task easyTask = createTask("easy",
+                DifficultyLevel.EASY,
+                Priority.LOW,
+                30,
+                null);
+        Task veryEasyTask = createTask("very easy",
+                DifficultyLevel.VERY_EASY,
+                Priority.LOW,
+                50,
+                null);
+        Task mediumTask = createTask("medium",
+                DifficultyLevel.MEDIUM,
+                Priority.LOW,
+                29,
+                null);
+        Task hardTask = createTask("hard",
+                DifficultyLevel.HARD,
+                Priority.HIGH,
+                40,
+                null);
+        Task veryHardTask = createTask("hard",
+                DifficultyLevel.VERY_HARD,
+                Priority.MEDIUM,
+                45,
+                null);
+
+        RecommendationScore recommendation = scoringEngine
+                .recommend(List.of(veryEasyTask, easyTask, mediumTask, veryHardTask, hardTask), mood)
+                .orElseThrow();
+
+        assertEquals("medium", recommendation.getTask().getTitle());
+    }
+
+    @Test
+    void shouldRecommendVeryHardTaskForEnergizedAndPeakUser(){
+
+        MoodEntry mood = createMood(MoodType.ENERGIZED, EnergyLevel.PEAK, 60);
+
+        Task easyTask = createTask("easy",
+                DifficultyLevel.EASY,
+                Priority.LOW,
+                30,
+                null);
+        Task veryEasyTask = createTask("very easy",
+                DifficultyLevel.VERY_EASY,
+                Priority.LOW,
+                50,
+                null);
+        Task mediumTask = createTask("medium",
+                DifficultyLevel.MEDIUM,
+                Priority.LOW,
+                29,
+                null);
+        Task hardTask = createTask("hard",
+                DifficultyLevel.HARD,
+                Priority.HIGH, 40,
+                null);
+        Task veryHardTask = createTask("very hard",
+                DifficultyLevel.VERY_HARD,
+                Priority.MEDIUM, 45,
+                null);
+
+        RecommendationScore recommendation = scoringEngine
+                .recommend(List.of(veryEasyTask, easyTask, mediumTask, veryHardTask, hardTask), mood)
+                .orElseThrow();
+
+        assertEquals("very hard", recommendation.getTask().getTitle());
+    }
+
+    @Test
+    void shouldRankTasksInDescendingOrder(){
+
+        MoodEntry mood = createMood(MoodType.ENERGIZED, EnergyLevel.HIGH, 120);
+
+        Task mediumTask = createTask("medium",
+                DifficultyLevel.MEDIUM,
+                Priority.HIGH,
+                100,
+                null);
+        Task hardTask = createTask("hard",
+                DifficultyLevel.HARD, Priority.LOW,
+                67,
+                null);
+        Task veryHardTask = createTask("very hard",
+                DifficultyLevel.VERY_HARD,
+                Priority.MEDIUM,
+                98,
+                null);
+
+        List<RecommendationScore> scores = scoringEngine.rankAll(List.of(hardTask, mediumTask, veryHardTask), mood);
+
+        assertTrue(scores.get(0).getScore() >= scores.get(1).getScore() &
+                scores.get(1).getScore() >= scores.get(2).getScore());
+    }
+
+    @Test
+    void shouldIgnoreCompletedTasks(){
+
+        MoodEntry mood = createMood(MoodType.NEUTRAL, EnergyLevel.HIGH, 240);
+
+        Task completed = createTask("completed",
+                DifficultyLevel.EASY,
+                Priority.MEDIUM,
+                80,
+                null);
+
+        completed.setCompleted("Completed");
+
+        Task notCompleted = createTask("not completed",
+                DifficultyLevel.HARD,
+                Priority.URGENT,
+                180,
+                null);
+
+        RecommendationScore recommendation = scoringEngine
+                .recommend(List.of(completed, notCompleted), mood)
+                        .orElseThrow();
+
+        assertEquals("not completed", recommendation.getTask().getTitle());
+    }
+
+    @Test
+    void shouldPrioritizeUrgentTask(){
+
+        MoodEntry mood = createMood(MoodType.FOCUSED, EnergyLevel.HIGH, 180);
+
+        Task urgentTask = createTask("urgent",
+                DifficultyLevel.MEDIUM,
+                Priority.URGENT,
+                120,
+                LocalDate.now().plusDays(7));
+
+        Task normalTask = createTask("normal",
+                DifficultyLevel.EASY,
+                Priority.MEDIUM,
+                120,
+                LocalDate.now().plusDays(4));
+
+        RecommendationScore recommend = scoringEngine
+                .recommend(List.of(urgentTask, normalTask), mood)
+                .orElseThrow();
+
+        assertEquals("urgent", recommend.getTask().getTitle());
+    }
+}
