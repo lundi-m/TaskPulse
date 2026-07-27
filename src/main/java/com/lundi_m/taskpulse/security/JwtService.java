a93560f0021e1c412b774decf9ff7edb4753610d
Lundi <keabetswelundim@gmail.com>
2026-07-27 23:28:17 +0200

    refactor: add @Service

--- src/main/java/com/lundi_m/taskpulse/security/JwtService.java
+++ src/main/java/com/lundi_m/taskpulse/security/JwtService.java
@@ -5,11 +5,12 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
-@Component
+@Service
 public class JwtService {
     @Value("${jwt.secret}")
     private String secret;
