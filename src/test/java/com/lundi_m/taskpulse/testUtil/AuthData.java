5911ec5c0e6a7f47bc759a1d4c2fca3a425f397a
Lundi <keabetswelundim@gmail.com>
2026-07-29 23:23:35 +0200

    test: add test data for register user request

--- src/test/java/com/lundi_m/taskpulse/testUtil/AuthData.java
+++ src/test/java/com/lundi_m/taskpulse/testUtil/AuthData.java
@@ -3,9 +3,21 @@ package com.lundi_m.taskpulse.testUtil;
 import com.lundi_m.taskpulse.dto.auth.AuthResponse;
 import com.lundi_m.taskpulse.dto.auth.LoginRequest;
 import com.lundi_m.taskpulse.dto.auth.RefreshRequest;
+import com.lundi_m.taskpulse.dto.auth.RegisterRequest;
 import com.lundi_m.taskpulse.dto.user.UserResponse;
 
 public class AuthData {
+
+    public static RegisterRequest createRegisterRequest(){
+        RegisterRequest request = new RegisterRequest();
+
+        request.setFirstName("Jane");
+        request.setLastName("Doe");
+        request.setEmail("janedoe@gmail.com");
+        request.setPassword("Found@Street213");
+
+        return request;
+    }
 
     public static UserResponse createUserResponse(){
         return UserResponse.builder()
