42e2575ec64cab3c4b4fadff34cfa126315c740b
Lundi <keabetswelundim@gmail.com>
2026-07-29 15:36:52 +0200

    test: add test data for authentication tests

---
+++ src/test/java/com/lundi_m/taskpulse/testUtil/AuthData.java
@@ -0,0 +1,41 @@
+package com.lundi_m.taskpulse.testUtil;
+
+import com.lundi_m.taskpulse.dto.auth.AuthResponse;
+import com.lundi_m.taskpulse.dto.auth.LoginRequest;
+import com.lundi_m.taskpulse.dto.auth.RefreshRequest;
+import com.lundi_m.taskpulse.dto.user.UserResponse;
+
+public class AuthData {
+
+    public static UserResponse createUserResponse(){
+        return UserResponse.builder()
+                .id(67L)
+                .firstName("Jane")
+                .lastName("Doe")
+                .email("janedoe@gmail.com")
+                .build();
+    }
+
+    public static LoginRequest createLoginRequest(){
+        LoginRequest request = new LoginRequest();
+        request.setEmail("janedoe@gmail.com");
+        request.setPassword("Missing@HELP123");
+
+        return request;
+    }
+
+    public static AuthResponse createAuthResponse(){
+        return AuthResponse.builder()
+                .accessToken("JaneDoeAccessToken")
+                .refreshToken("JaneDoeRefreshToken")
+                .build();
+    }
+
+    public static RefreshRequest createRefreshRequest(){
+
+        RefreshRequest request = new RefreshRequest();
+        request.setRefreshToken("JaneDoeRefreshToken");
+
+        return request;
+    }
+}
